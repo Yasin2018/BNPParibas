cc946da3c08c52c730316cd69fe7f4623df23859
Yasin2018 <43721835+Yasin2018@users.noreply.github.com>
2020-10-17 09:39:42 +0800

    Update WeatherRepository.java

--- src/main/java/com/hackerrank/weather/repository/WeatherRepository.java
+++ src/main/java/com/hackerrank/weather/repository/WeatherRepository.java
@@ -1,4 +1,7 @@
 package com.hackerrank.weather.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.hackerrank.weather.model.Weather;
+public interface WeatherRepository extends JpaRepository<Weather,Long> {
 
-public interface WeatherRepository {
+
 }
